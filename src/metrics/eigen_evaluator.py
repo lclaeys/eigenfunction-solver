1068b2eb1de9eb2848d0b076ca06384e49ba884d
Louis C <louis.claeys@live.be>
2025-01-07 12:37:55 +0100

    clean up plotting code

--- src/metrics/eigen_evaluator.py
+++ src/metrics/eigen_evaluator.py
@@ -75,7 +75,7 @@ class EigenEvaluator():
                     x_mu = self.rng.choice(samples,solver.num_samples,axis=0)
                     grad_fx = solver.predict_grad(x_mu)[:,:k,:]
                 
-                costs = np.diag(np.sum(np.matmul(grad_fx,np.transpose(grad_fx,axes=[0,2,1])),axis=0))
+                costs = np.diag(np.mean(np.matmul(grad_fx,np.transpose(grad_fx,axes=[0,2,1])),axis=0))
                 out[metric] = np.cumsum(costs)
 
             if metric == "eigenvalue_mse":
@@ -174,18 +174,21 @@ class EigenEvaluator():
                 
         return out
     
-    def plot_eigfuncs_exact(self, solver, x, k):
+    def plot(self, solver, x, k, plot_exact = False, plot_Lf = False):
         """
         Plot the first k eigenfunctions computed by the given solver at a grid defined using 5 and 95$ quantiles of x in each dimension.
-        The plot shows:
-            - plotted learned eigenfunction
-            - plotted true eigenfunction
-            - plotted Lf_i/lambda_i for each learned eigenfunction
+
+        Only works for d=1 or d=2.
         
         Args:
             solver (BaseSolver): fitted solver object
             x (ndarray): points
             k (int): number of eigfuncs to plot
+            plot_exact (bool): plot exact eigenfunctions (should be supported by energy)
+            plot_Lf (bool): plot Lf/lambda of fitted eigenfunctions (should be supported by solver)
+
+        Returns:
+            fig, ax: figure objects
         """
 
         if solver.dim == 1:
@@ -193,245 +196,65 @@ class EigenEvaluator():
             t = np.linspace(tmin, tmax,1000)[:,None]
             
             fx = solver.predict(t)[:,:k]
-            Lfx = solver.predict_Lf(t)[:,:k]
+            
+            if plot_Lf:
+                Lfx = solver.predict_Lf(t)[:,:k]
 
             i = 0
-            eigvals = self.energy.exact_eigvals(k)
-            eigfuncs =  self.energy.exact_eigfunctions(t, k)
+            
+
             rotated_fx = np.zeros_like(fx)
-            rotated_Lfx = np.zeros_like(Lfx)
-            while i < k:
-                cur_eigval = eigvals[i]
-                e = 1
-                j = 1
-                while i + j < k and eigvals[i+j] == cur_eigval:
-                    e += 1
-                    j += 1
-                # i = first index of eigenspace
-                # i + j - 1 = last index of eigenspace
-                # e = dimension of eigenspace
-                F = eigfuncs[:,i:i+j]
-                Fhat = fx[:,i:i+j]
+            
+            if plot_Lf:
+                rotated_Lfx = np.zeros_like(Lfx)
+                fitted_eigvals = solver.fit_eigvals(x)
 
-                R = self.solve_procrustes(Fhat, F)
-                rotated_Fhat = Fhat@R             
-                rotated_fx[:,i:i+j] = rotated_Fhat
-                rotated_Lfx[:,i:i+j] = Lfx[:,i:i+j]@R
+            # rotate eigenfunctions to match exact solution
+            if plot_exact:
+                eigvals = self.energy.exact_eigvals(k)
+                eigfuncs =  self.energy.exact_eigfunctions(t, k)
+                while i < k:
+                    cur_eigval = eigvals[i]
+                    e = 1
+                    j = 1
+                    while i + j < k and eigvals[i+j] == cur_eigval:
+                        e += 1
+                        j += 1
+                    # i = first index of eigenspace
+                    # i + j - 1 = last index of eigenspace
+                    # e = dimension of eigenspace
+                    F = eigfuncs[:,i:i+j]
+                    Fhat = fx[:,i:i+j]
 
-                i = i + j
+                    R = self.solve_procrustes(Fhat, F)
+                    rotated_Fhat = Fhat@R             
+                    rotated_fx[:,i:i+j] = rotated_Fhat
+                    if plot_Lf:
+                        rotated_Lfx[:,i:i+j] = Lfx[:,i:i+j]@R
+
+                    i = i + j
+
+                fx = rotated_fx
+                if plot_Lf:
+                    Lfx = rotated_Lfx
 
             fig, ax = plt.subplots(k,1,figsize=(10,5*k))
-            fitted_eigvals = solver.fit_eigvals(x)
-            for i in range(k):
-                ax[i].plot(t,eigfuncs[:,i],lw=2,color='black',label='true')
-                ax[i].plot(t,rotated_fx[:,i],lw=2,color='blue',label='pred')
-                ax[i].plot(t,rotated_Lfx[:,i]/fitted_eigvals[i],lw=2,color='red',label='Lfx/lambda')
-                ax[i].legend()
-                ax[i].set_title(f'True eigval: {eigvals[i]}. Predicted eigval: {solver.eigvals[i]:.3f}. Fitted eigval: {fitted_eigvals[i]:.3f}')
-        
-            return fig, ax
-        
-        elif solver.dim == 2:
-            tmin, tmax = np.quantile(x[:,0], [0.05,0.95])
-            tx = np.linspace(tmin, tmax,100)[:,None]
-            tmin, tmax = np.quantile(x[:,1], [0.05,0.95])
-            ty = np.linspace(tmin, tmax,100)[:,None]
-
-            tx, ty = np.meshgrid(tx, ty)
-            # Reshape the grid into an Nx2 array for the function
-            grid = np.stack([tx.ravel(), ty.ravel()], axis=-1)
-
-            fx = solver.predict(grid)[:,:k]
-            Lfx = solver.predict_Lf(grid)[:,:k]
-
-            i = 0
-            eigvals = self.energy.exact_eigvals(k)
-            eigfuncs =  self.energy.exact_eigfunctions(grid, k)
-            rotated_fx = np.zeros_like(fx)
-            rotated_Lfx = np.zeros_like(Lfx)
-            while i < k:
-                cur_eigval = eigvals[i]
-                e = 1
-                j = 1
-                while i + j < k and eigvals[i+j] == cur_eigval:
-                    e += 1
-                    j += 1
-                # i = first index of eigenspace
-                # i + j - 1 = last index of eigenspace
-                # e = dimension of eigenspace
-                F = eigfuncs[:,i:i+j]
-                Fhat = fx[:,i:i+j]
-
-                R = self.solve_procrustes(Fhat, F)
-                rotated_Fhat = Fhat@R             
-                rotated_fx[:,i:i+j] = rotated_Fhat
-                rotated_Lfx[:,i:i+j] = Lfx[:,i:i+j]@R
-
-                i = i + j
-            
-            fig, ax = plt.subplots(k,3,figsize=(10,5*k),sharey=True)
-            fitted_eigvals = solver.fit_eigvals(x)
             
             for i in range(k):
-                zhat = rotated_fx[:,i].reshape(tx.shape)
-                Lzhat = rotated_Lfx[:,i].reshape(tx.shape)/fitted_eigvals[i]
-                z = eigfuncs[:,i].reshape(tx.shape)
-                # Determine the global min and max for consistent color mapping
-                z_min = min(z.min(), zhat.min(), Lzhat.min())
-                z_max = max(z.max(), zhat.max(), Lzhat.max())
-
-                # Plot first contour
-                contour1 = ax[i,0].contourf(tx, ty, zhat, levels=np.linspace(z_min, z_max, 10), cmap='viridis')
-                ax[i,0].set_title(f'Predicted eigval: {solver.eigvals[i]:.3f}')
-
-                # Plot second contour
-                contour2 = ax[i,1].contourf(tx, ty, z, levels=np.linspace(z_min, z_max, 10), cmap='viridis')
-                ax[i,1].set_title(f'True eigval: {eigvals[i]}')
-
-                # Plot third contour
-                contour3 = ax[i,2].contourf(tx, ty, Lzhat, levels=np.linspace(z_min, z_max, 10), cmap='viridis')
-                ax[i,2].set_title(f'Fitted eigval: {fitted_eigvals[i]:.3f}')
-
-                # Add a shared colorbar
-                fig.colorbar(contour1, ax=ax[i,:], orientation='vertical', label='Function Value')
-
-            return fig, ax
-        
-    def plot_eigfuncs_exact_noL(self, solver, x, k):
-        """
-        Plot the first k eigenfunctions computed by the given solver at a grid defined using 5 and 95$ quantiles of x in each dimension.
-        The plot shows:
-            - plotted learned eigenfunction
-            - plotted true eigenfunction
-        
-        Args:
-            solver (BaseSolver): fitted solver object
-            x (ndarray): points
-            k (int): number of eigfuncs to plot
-        """
-
-        if solver.dim == 1:
-            tmin, tmax = np.quantile(x, [0.05,0.95])
-            t = np.linspace(tmin, tmax,1000)[:,None]
-            
-            fx = solver.predict(t)[:,:k]
-
-            i = 0
-            eigvals = self.energy.exact_eigvals(k)
-            eigfuncs =  self.energy.exact_eigfunctions(t, k)
-            rotated_fx = np.zeros_like(fx)
-            while i < k:
-                cur_eigval = eigvals[i]
-                e = 1
-                j = 1
-                while i + j < k and eigvals[i+j] == cur_eigval:
-                    e += 1
-                    j += 1
-                # i = first index of eigenspace
-                # i + j - 1 = last index of eigenspace
-                # e = dimension of eigenspace
-                F = eigfuncs[:,i:i+j]
-                Fhat = fx[:,i:i+j]
-
-                R = self.solve_procrustes(Fhat, F)
-                rotated_Fhat = Fhat@R             
-                rotated_fx[:,i:i+j] = rotated_Fhat
-
-                i = i + j
-
-            fig, ax = plt.subplots(k,1,figsize=(10,5*k))
-            for i in range(k):
-                ax[i].plot(t,eigfuncs[:,i],lw=2,color='black',label='true')
-                ax[i].plot(t,rotated_fx[:,i],lw=2,color='blue',label='pred')
-                ax[i].legend()
-                ax[i].set_title(f'True eigval: {eigvals[i]}. Predicted eigval: {solver.eigvals[i]:.3f}')
-        
-            return fig, ax
-        
-        elif solver.dim == 2:
-            tmin, tmax = np.quantile(x[:,0], [0.05,0.95])
-            tx = np.linspace(tmin, tmax,100)[:,None]
-            tmin, tmax = np.quantile(x[:,1], [0.05,0.95])
-            ty = np.linspace(tmin, tmax,100)[:,None]
-
-            tx, ty = np.meshgrid(tx, ty)
-            # Reshape the grid into an Nx2 array for the function
-            grid = np.stack([tx.ravel(), ty.ravel()], axis=-1)
-
-            fx = solver.predict(grid)[:,:k]
-
-            i = 0
-            eigvals = self.energy.exact_eigvals(k)
-            eigfuncs =  self.energy.exact_eigfunctions(grid, k)
-            rotated_fx = np.zeros_like(fx)
-            while i < k:
-                cur_eigval = eigvals[i]
-                e = 1
-                j = 1
-                while i + j < k and eigvals[i+j] == cur_eigval:
-                    e += 1
-                    j += 1
-                # i = first index of eigenspace
-                # i + j - 1 = last index of eigenspace
-                # e = dimension of eigenspace
-                F = eigfuncs[:,i:i+j]
-                Fhat = fx[:,i:i+j]
-
-                R = self.solve_procrustes(Fhat, F)
-                rotated_Fhat = Fhat@R             
-                rotated_fx[:,i:i+j] = rotated_Fhat
-
-                i = i + j
-            
-            fig, ax = plt.subplots(k,2,figsize=(10,5*k),sharey=True)
-            
-            for i in range(k):
-                zhat = rotated_fx[:,i].reshape(tx.shape)
-                z = eigfuncs[:,i].reshape(tx.shape)
-                # Determine the global min and max for consistent color mapping
-                z_min = min(z.min(), zhat.min()) - 1e-6
-                z_max = max(z.max(), zhat.max()) + 1e-6
-
-                # Plot first contour
-                contour1 = ax[i,0].contourf(tx, ty, zhat, levels=np.linspace(z_min, z_max, 10), cmap='viridis')
-                ax[i,0].set_title(f'Predicted eigval: {solver.eigvals[i]:.3f}')
-
-                # Plot second contour
-                contour2 = ax[i,1].contourf(tx, ty, z, levels=np.linspace(z_min, z_max, 10), cmap='viridis')
-                ax[i,1].set_title(f'True eigval: {eigvals[i]}')
-
-                # Add a shared colorbar
-                fig.colorbar(contour1, ax=ax[i,:], orientation='vertical', label='Function Value')
-
-            return fig, ax
-        
-    def plot_eigfuncs(self, solver, x, k):
-        """
-        Plot the first k eigenfunctions computed by the given solver at a grid defined using 5 and 95$ quantiles of x in each dimension.
-        The plot shows:
-            - plotted learned eigenfunction
-            - plotted Lf_i/lambda_i for each learned eigenfunction
-        
-        Args:
-            solver (BaseSolver): fitted solver object
-            x (ndarray): points
-            k (int): number of eigfuncs to plot
-        """
-
-        if solver.dim == 1:
-            tmin, tmax = np.quantile(x, [0.05,0.95])
-            t = np.linspace(tmin, tmax,1000)[:,None]
-            
-            fx = solver.predict(t)[:,:k]
-            Lfx = solver.predict_Lf(t)[:,:k]
-            
-            fig, ax = plt.subplots(k,1,figsize=(10,5*k))
-            fitted_eigvals = solver.fit_eigvals(x)
-            for i in range(k):
+                title = ""
+                if plot_exact:
+                    ax[i].plot(t,eigfuncs[:,i],lw=2,color='black',label='true')
+                    title += f"True eigval: {eigvals[i]:.3f}. "
+                
+                if plot_Lf:
+                    ax[i].plot(t,Lfx[:,i]/fitted_eigvals[i],lw=2,color='red',label='Lfx/lambda')
+                    title += f"Fitted eigval: {fitted_eigvals[i]:.3f}. "
+                
                 ax[i].plot(t,fx[:,i],lw=2,color='blue',label='pred')
-                ax[i].plot(t,Lfx[:,i]/fitted_eigvals[i],lw=2,color='red',label='Lfx/lambda')
+                title += f"Method eigval: {solver.eigvals[i]:.3f}."
+                
                 ax[i].legend()
-                ax[i].set_title(f'Predicted eigval: {solver.eigvals[i]:.3f}. Fitted eigval: {fitted_eigvals[i]:.3f}')
+                ax[i].set_title(title)
         
             return fig, ax
         
@@ -446,32 +269,79 @@ class EigenEvaluator():
             grid = np.stack([tx.ravel(), ty.ravel()], axis=-1)
 
             fx = solver.predict(grid)[:,:k]
-            Lfx = solver.predict_Lf(grid)[:,:k]
 
-            fig, ax = plt.subplots(k,2,figsize=(10,5*k),sharey=True)
-            fitted_eigvals = solver.fit_eigvals(x)
+            if plot_Lf:
+                Lfx = solver.predict_Lf(grid)[:,:k]
+                fitted_eigvals = solver.fit_eigvals(x)
+                rotated_Lfx = np.zeros_like(Lfx)
+
+            if plot_exact:
+                eigvals = self.energy.exact_eigvals(k)
+                eigfuncs =  self.energy.exact_eigfunctions(grid, k)
+                i = 0
+
+                rotated_fx = np.zeros_like(fx)
+                
+                while i < k:
+                    cur_eigval = eigvals[i]
+                    e = 1
+                    j = 1
+                    while i + j < k and eigvals[i+j] == cur_eigval:
+                        e += 1
+                        j += 1
+                    # i = first index of eigenspace
+                    # i + j - 1 = last index of eigenspace
+                    # e = dimension of eigenspace
+                    F = eigfuncs[:,i:i+j]
+                    Fhat = fx[:,i:i+j]
+
+                    R = self.solve_procrustes(Fhat, F)
+                    rotated_Fhat = Fhat@R             
+                    rotated_fx[:,i:i+j] = rotated_Fhat
+                    if plot_Lf:
+                        rotated_Lfx[:,i:i+j] = Lfx[:,i:i+j]@R
+
+                    i = i + j
+                fx = rotated_fx
+                if plot_Lf:
+                    Lfx = rotated_Lfx
+            
+            num_plots = 1 + int(plot_Lf) + int(plot_exact)
+            fig, ax = plt.subplots(k,num_plots,figsize=(num_plots*4,5*k),sharey=True)
             
             for i in range(k):
-                zhat = fx[:,i].reshape(tx.shape)
-                Lzhat = Lfx[:,i].reshape(tx.shape)/fitted_eigvals[i]
+                zvalues = []
+                titles = []
+
+                zvalues.append(fx[:,i].reshape(tx.shape))
+                titles.append(f'Predicted eigval: {solver.eigvals[i]:.3f}')
+
+                if plot_Lf:
+                    zvalues.append(Lfx[:,i].reshape(tx.shape)/fitted_eigvals[i])
+                    titles.append(f'Fitted eigval: {fitted_eigvals[i]:.3f}')
+                
+                if plot_exact:
+                    zvalues.append(eigfuncs[:,i].reshape(tx.shape))
+                    titles.append(f'True eigval: {eigvals[i]}')
+                
+
                 # Determine the global min and max for consistent color mapping
-                z_min = zhat.min()
-                z_max = zhat.max()
+                z_min = min([z.min() for z in zvalues])
+                z_max = max([z.max() for z in zvalues])
 
-                # Plot first contour
-                contour1 = ax[i,0].contourf(tx, ty, zhat, levels=np.linspace(z_min, z_max, 30), cmap='viridis')
-                ax[i,0].set_title(f'Predicted eigval: {solver.eigvals[i]:.3f}')
+                if num_plots > 1:
+                    for j in range(num_plots):
+                        contour = ax[i,j].contourf(tx, ty, zvalues[j], levels=np.linspace(z_min, z_max, 10), cmap='viridis')
+                        ax[i,j].set_title(titles[j])
+                    fig.colorbar(contour, ax=ax[i,:], orientation='vertical', label='Function Value')
 
-                # Plot second contour
-                contour2 = ax[i,1].contourf(tx, ty, Lzhat, levels=np.linspace(z_min, z_max, 30), cmap='viridis')
-                ax[i,1].set_title(f'Fitted eigval: {fitted_eigvals[i]:.3f}')
-
-
-                # Add a shared colorbar
-                fig.colorbar(contour1, ax=ax[i,:], orientation='vertical', label='Function Value')
+                else:
+                    contour = ax[i].contourf(tx, ty, zvalues[0], levels=np.linspace(z_min, z_max, 10), cmap='viridis')
+                    ax[i].set_title(titles[0])
+                    fig.colorbar(contour, ax=ax[i], orientation='vertical', label='Function Value')
 
             return fig, ax
-        
+    
     @staticmethod
     def solve_procrustes(A, B):
         """
